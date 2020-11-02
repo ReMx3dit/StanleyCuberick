4aa8fead331959018e4806fe5f686ca3722ae0f9
Robin Van Gijseghem <robin@vangijseghem.be>
2020-11-02 22:20:28 +0100

    Added project to repo

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public int restartDelayInSeconds = 3;
+    public GameObject completeLevelUI;
+    public GameObject failedLevelUI;
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+    public void EndGame()
+    {
+        if (gameHasEnded == false)
+        {
+            failedLevelUI.SetActive(true);
+            gameHasEnded = true;
+            Invoke("Restart", restartDelayInSeconds);
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody player;
+    public float forwardVelocity;
+    public float sidewaysVelocity;
+    bool KeyLeft;
+    bool KeyRight;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A)) KeyLeft = true;
+        else KeyLeft = false;
+
+        if (Input.GetKey(KeyCode.D)) KeyRight = true;
+        else KeyRight = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        player.AddForce(0, 0, forwardVelocity * Time.deltaTime);
+
+        if (KeyLeft)
+        {
+            player.AddForce(-sidewaysVelocity * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (KeyRight)
+        {
+            player.AddForce(sidewaysVelocity * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (player.position.y < -1)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
